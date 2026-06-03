8c9ca4dfc9a12632e99aa4e99626b077556d5589
Daryl Rowe <daryl_rowe@hotmail.com>
2026-06-03 19:04:57 -0400

    fix indentation and from address

--- email_utils.py
+++ email_utils.py
@@ -10,15 +10,16 @@ SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+VERIFIED_SENDER = os.getenv("VERIFIED_SENDER")
 
 def send_email(to_email, subject, body):
     msg = MIMEText(body)
     msg["Subject"] = subject
-    msg["From"] = SMTP_USER
+    msg["From"] = VERIFIED_SENDER
     msg["To"] = to_email
 
     server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     server.starttls()
     server.login(SMTP_USER, SMTP_PASSWORD)
-    server.sendmail(SMTP_USER, to_email, msg.as_string())
+    server.sendmail(VERIFIED_SENDER, to_email, msg.as_string())
     server.quit()
